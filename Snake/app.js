89103463feecad33cff410b602642326f0634a95
Tatiana Racheva <tatiana.racheva@gmail.com>
2013-12-15 00:50:52 -0800

    Translated everything, but step doesn't work yet
    Need to debug to figure out what's happening on step.

--- Snake/app.js
+++ Snake/app.js
@@ -1,32 +1,6 @@
 /// <reference path="jquery.d.ts" />
 var GameBoardId = "game-board";
 
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-
-        $("<canvas/>", {
-            id: GameBoardId,
-            html: "This should look like a game board"
-        }).appendTo(this.element);
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
-    };
-
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-})();
-
 var Cell = (function () {
     function Cell(x, y) {
         this.x = x;
@@ -37,10 +11,11 @@ var Cell = (function () {
 
 var Direction;
 (function (Direction) {
-    Direction[Direction["UP"] = 0] = "UP";
-    Direction[Direction["DOWN"] = 1] = "DOWN";
-    Direction[Direction["LEFT"] = 2] = "LEFT";
-    Direction[Direction["RIGHT"] = 3] = "RIGHT";
+    Direction[Direction["NONE"] = 0] = "NONE";
+    Direction[Direction["UP"] = 1] = "UP";
+    Direction[Direction["DOWN"] = 2] = "DOWN";
+    Direction[Direction["LEFT"] = 3] = "LEFT";
+    Direction[Direction["RIGHT"] = 4] = "RIGHT";
 })(Direction || (Direction = {}));
 
 var Snake = (function () {
@@ -48,65 +23,329 @@ var Snake = (function () {
         this.body = body;
         this.direction = direction;
     }
+    Object.defineProperty(Snake.prototype, "head", {
+        get: function () {
+            return this.body[0];
+        },
+        enumerable: true,
+        configurable: true
+    });
+
+    Object.defineProperty(Snake.prototype, "tail", {
+        get: function () {
+            return this.body[this.body.length - 1];
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Snake;
 })();
 
 var Game = (function () {
-    function Game(panel, cellSize, lengthToWin, msPerMove, initialApple, snake) {
+    function Game(panel, cellSize, lengthToWin, msPerMove, apple, snake) {
         this.panel = panel;
         this.cellSize = cellSize;
         this.lengthToWin = lengthToWin;
         this.msPerMove = msPerMove;
-        this.initialApple = initialApple;
+        this.apple = apple;
         this.snake = snake;
     }
     return Game;
 })();
 
-function boardDimentions(panel, cellSize) {
+// NOTE: all these functions that take the 1st parameter of Game or Panel should be instance methods.
+function createCenterCell(dimensions) {
+    return new Cell(Math.floor(dimensions.width / 2), Math.floor(dimensions.height / 2));
+}
+
+function createRandomCell(dimensions) {
+    return new Cell(Math.floor((Math.random() * dimensions.width)), Math.floor((Math.random() * dimensions.height)));
+}
+
+function paintCell(panel, color, cellSize, cell) {
+    if (!panel.getContext) {
+        return;
+    }
+
+    var ctx = panel.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(cellSize * cell.x, cellSize * cell.y, cellSize, cellSize);
+}
+
+// Static game
+function boardDimensions(panel, cellSize) {
     return {
         width: Math.floor($(panel).width() / cellSize), height: Math.floor($(panel).height() / cellSize)
     };
 }
 
-function createCenterCell(width, heigth) {
-    return new Cell(Math.floor(width / 2), Math.floor(heigth / 2));
-}
-
-function createRandomCell(width, height) {
-    return new Cell(Math.floor((Math.random() * width)), Math.floor((Math.random() * height)));
-}
-
-function createSnake(width, height) {
-    var head = createCenterCell(width, height);
+// Static Game
+function createSnake(dimensions) {
+    var head = createCenterCell(dimensions);
     var body = [head];
-    return new Snake(body, 3 /* RIGHT */);
+    return new Snake(body, 4 /* RIGHT */);
 }
 
+// Game constuctor overload?
 function createGame(panel, cellSize) {
     var lengthToWin = 10;
     var msPerMove = 50;
-    var dimentions = boardDimentions(panel, cellSize);
-    var apple = createRandomCell(dimentions.width, dimentions.height);
-    var snake = createSnake(dimentions.width, dimentions.height);
+    var dimensions = boardDimensions(panel, cellSize);
+    var apple = createRandomCell(dimensions);
+    var snake = createSnake(dimensions);
     return new Game(panel, cellSize, lengthToWin, msPerMove, apple, snake);
 }
 
-function paintCell(panel, color, cellSize, cell) {
-    var canvas = document.getElementById(GameBoardId);
+// Instance game
+function eraseCell(game, cell) {
+    paintCell(game.panel, $(game.panel).css("background-color"), game.cellSize, cell);
+}
 
-    if (!canvas.getContext) {
-        return;
+// Instance game
+function eraseApple(game) {
+    eraseCell(game, game.apple);
+}
+
+// Instance game
+function eraseSnake(game) {
+    game.snake.body.forEach(function (cell) {
+        eraseCell(game, cell);
+    });
+}
+
+// Static game
+function paintApple(panel, cellSize, apple) {
+    paintCell(panel, "red", cellSize, apple);
+}
+
+function paintSnake(panel, cellSize, snake) {
+    // We only need to paint the head because the rest will have been already painted.
+    paintCell(panel, "green", cellSize, snake.head);
+}
+
+function paintGame(game) {
+    paintApple(game.panel, game.cellSize, game.apple);
+    paintSnake(game.panel, game.cellSize, game.snake);
+}
+
+function newApple(game) {
+    eraseApple(game);
+    return createRandomCell(boardDimensions(game.panel, game.cellSize));
+}
+
+var deltaVector = new Array();
+deltaVector[3 /* LEFT */] = { dx: -1, dy: 0 };
+deltaVector[4 /* RIGHT */] = { dx: 1, dy: 0 };
+deltaVector[1 /* UP */] = { dx: 0, dy: -1 };
+deltaVector[2 /* DOWN */] = { dx: 0, dy: 1 };
+
+// Gets a vector containing dx and dy values for a given direction
+function delta(direction) {
+    return deltaVector[direction];
+}
+
+// Returns the snake's direction, which is either the current direction or a new one if a board edge was reached.
+// NOTE: or this may indicate the end of the game. No?
+function newDirection(game) {
+    var snake = game.snake;
+    var direction = snake.direction;
+    var head = snake.head;
+    var x = head.x;
+    var y = head.y;
+
+    // TODO: this is annoying. This should be cached in a game's property.
+    var dimensions = boardDimensions(game.panel, game.cellSize);
+    var atLeft = (x === 0);
+    var atRight = (x === (dimensions.width - 1));
+    var atTop = (y === 0);
+    var atBottom = (y === (dimensions.height - 1));
+
+    // Turn clockwise when a board edge is reached unless that would result in going off the board
+    if (direction === 1 /* UP */ && atTop) {
+        return atRight ? 3 /* LEFT */ : 4 /* RIGHT */;
     }
 
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = color;
-    ctx.fillRect(cellSize * cell.x, cellSize * cell.y, cellSize, cellSize);
+    if (direction === 4 /* RIGHT */ && atRight) {
+        return atBottom ? 1 /* UP */ : 2 /* DOWN */;
+    }
+
+    if (direction === 2 /* DOWN */ && atBottom) {
+        return atLeft ? 4 /* RIGHT */ : 3 /* LEFT */;
+    }
+
+    if (direction === 3 /* LEFT */ && atLeft) {
+        return atTop ? 2 /* DOWN */ : 1 /* UP */;
+    }
+
+    return direction;
 }
+
+function isSameOrAdjacentCell(cell1, cell2) {
+    var dx = Math.abs(cell1.x - cell2.x);
+    var dy = Math.abs(cell2.y - cell2.y);
+
+    return dx <= 1 && dy <= 1;
+}
+
+function shouldEatApple(game) {
+    return isSameOrAdjacentCell(game.snake.head, game.apple);
+}
+
+function removeTail(game, body) {
+    eraseCell(game, game.snake.tail);
+    return body.slice(0, body.length - 1);
+}
+
+// Moves the snake and returns a new snake class. The snake grows by one cell if grow is true.
+function moveSnake(game, grow) {
+    var direction = newDirection(game);
+    var d = delta(direction);
+    var head = game.snake.head;
+    var newHead = new Cell(head.x + d.dx, head.y + d.dy);
+    var body = game.snake.body.concat([newHead], game.snake.body);
+    if (!grow) {
+        body = removeTail(game, body);
+    }
+
+    return new Snake(body, direction);
+}
+
+// Gets a direction that describes the direction associated with a given key code.
+function getKeyDirection(keyCode) {
+    switch (keyCode) {
+        case 37:
+            return 3 /* LEFT */;
+        case 38:
+            return 1 /* UP */;
+        case 39:
+            return 4 /* RIGHT */;
+        case 40:
+            return 2 /* DOWN */;
+    }
+    return 0 /* NONE */;
+}
+
+function isValidChange(keyDirection, currentDirection) {
+    switch (keyDirection) {
+        case 0 /* NONE */:
+            return false;
+        case 3 /* LEFT */:
+            return currentDirection !== 4 /* RIGHT */;
+        case 4 /* RIGHT */:
+            return currentDirection !== 3 /* LEFT */;
+        case 1 /* UP */:
+            return currentDirection !== 2 /* DOWN */;
+        case 2 /* DOWN */:
+            return currentDirection !== 1 /* UP */;
+    }
+
+    return true;
+}
+
+var globalKeyCode;
+
+function snakeWithKeyDirection(snake) {
+    var keyDirection = getKeyDirection(globalKeyCode);
+    var current = snake.direction;
+
+    // Don't let the snake double back on itself.
+    if (isValidChange(keyDirection, current)) {
+        globalKeyCode = null;
+        snake.direction = keyDirection;
+    }
+
+    return snake;
+}
+
+function doesHeadOverlapBody(body) {
+    var head = body[0];
+    for (var i = 1; i < body.length; i++) {
+        if (head === body[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function restartGame(game) {
+    eraseApple(game);
+    eraseSnake(game);
+    return createGame(game.panel, game.cellSize);
+}
+
+function newGame(game, message) {
+    var panel = game.panel;
+    //alert(message);
+    //return restartGame(game);
+}
+
+function didWin(game) {
+    return (game.snake.body.length === game.lengthToWin);
+}
+
+function didLose(game) {
+    return doesHeadOverlapBody(game.snake.body);
+}
+
+function step(game) {
+    var eat = shouldEatApple(game);
+    var snake = snakeWithKeyDirection(game.snake);
+    game.snake = snake;
+    if (eat) {
+        game.apple = newApple(game);
+    }
+    snake = moveSnake(game, eat);
+
+    if (didLose(game)) {
+        return newGame(game, "You killed the snake!");
+    } else if (didWin) {
+        return newGame(game, "You win!");
+    } else {
+        game.snake = snake;
+    }
+
+    return game;
+}
+
+function configurePanel(panel) {
+    $(panel).attr("tabindex", "0").on("keydown", function (ev) {
+        globalKeyCode = ev.keyCode;
+        return false;
+    });
+}
+
+var Greeter = (function () {
+    function Greeter(element) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+
+        var gameBoard = document.getElementById(GameBoardId);
+
+        var cellSize = 20;
+        this.game = createGame(document.getElementById(GameBoardId), cellSize);
+        paintGame(this.game);
+    }
+    Greeter.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () {
+            step(_this.game);
+        }, 500);
+    };
+
+    Greeter.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return Greeter;
+})();
+
+var greeter;
 
 window.onload = function () {
     var el = document.getElementById("content");
-    var greeter = new Greeter(el);
+    greeter = new Greeter(el);
     greeter.start();
 };
 //# sourceMappingURL=app.js.map
--- Snake/app.ts
+++ Snake/app.ts
@@ -2,40 +2,13 @@
 
 var GameBoardId = "game-board";
 
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-
-        $("<canvas/>", {
-            id: GameBoardId,
-            html: "This should look like a game board"
-        }).appendTo(this.element);
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
 class Cell {
     constructor(public x: number, public y: number) {
     }
 }
 
 enum Direction {
+    NONE,
     UP,
     DOWN,
     LEFT,
@@ -43,64 +16,316 @@ enum Direction {
 }
 
 class Snake {
-    constructor(public body: any, public direction: Direction) {
+    constructor(public body: Cell[], public direction: Direction) {
+    }
+
+    get head(): Cell {
+        return this.body[0];
+    }
+
+    get tail(): Cell {
+        return this.body[this.body.length - 1];
     }
 }
 
 class Game {
     constructor(
-        public panel: HTMLElement,
+        public panel: HTMLCanvasElement,
         public cellSize: number,
         public lengthToWin: number,
         public msPerMove: number,
-        public initialApple: Cell,
+        public apple: Cell,
         public snake: Snake) {
     }
 }
 
-function boardDimentions(panel: HTMLElement, cellSize: number) {
+// NOTE: all these functions that take the 1st parameter of Game or Panel should be instance methods.
+
+function createCenterCell(dimensions: { width: number; height: number }) {
+    return new Cell(Math.floor(dimensions.width / 2), Math.floor(dimensions.height / 2));
+}
+
+function createRandomCell(dimensions: { width: number; height: number }) {
+    return new Cell(Math.floor((Math.random() * dimensions.width)), Math.floor((Math.random() * dimensions.height)));
+}
+
+function paintCell(panel: HTMLCanvasElement, color: string, cellSize: number, cell: Cell) {
+    if (!panel.getContext) {
+        return;
+    }
+
+    var ctx = panel.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(cellSize * cell.x, cellSize * cell.y, cellSize, cellSize);
+}
+
+// Static game
+function boardDimensions(panel: HTMLElement, cellSize: number) {
     return {
         width: Math.floor($(panel).width() / cellSize), height: Math.floor($(panel).height() / cellSize)
     };
 }
 
-function createCenterCell(width: number, heigth: number) {
-    return new Cell(Math.floor(width / 2), Math.floor(heigth / 2));
-}
-
-function createRandomCell(width: number, height: number) {
-    return new Cell(Math.floor((Math.random() * width)), Math.floor((Math.random() * height)));
-}
-
-function createSnake(width: number, height: number) {
-    var head = createCenterCell(width, height);
+// Static Game
+function createSnake(dimensions: { width: number; height: number }) {
+    var head = createCenterCell(dimensions);
     var body = [head];
     return new Snake(body, Direction.RIGHT);
 }
 
-function createGame(panel: HTMLElement, cellSize: number) {
+// Game constuctor overload?
+function createGame(panel: HTMLCanvasElement, cellSize: number) {
     var lengthToWin = 10;
     var msPerMove = 50;
-    var dimentions = boardDimentions(panel, cellSize);
-    var apple = createRandomCell(dimentions.width, dimentions.height);
-    var snake = createSnake(dimentions.width, dimentions.height);
+    var dimensions = boardDimensions(panel, cellSize);
+    var apple = createRandomCell(dimensions);
+    var snake = createSnake(dimensions);
     return new Game(panel, cellSize, lengthToWin, msPerMove, apple, snake);
 }
 
-function paintCell(panel: HTMLElement, color: string, cellSize: number, cell: Cell) {
-    var canvas = <HTMLCanvasElement> document.getElementById(GameBoardId);
+// Instance game
+function eraseCell(game: Game, cell: Cell) {
+    paintCell(game.panel, $(game.panel).css("background-color"), game.cellSize, cell);
+}
 
-    if (!canvas.getContext) {
-        return;
+// Instance game
+function eraseApple(game: Game) {
+    eraseCell(game, game.apple);
+}
+
+// Instance game
+function eraseSnake(game: Game) {
+    game.snake.body.forEach(function (cell: Cell) {
+        eraseCell(game, cell);
+    });
+}
+
+// Static game
+function paintApple(panel: HTMLCanvasElement, cellSize: number, apple: Cell) {
+    paintCell(panel, "red", cellSize, apple);
+}
+
+function paintSnake(panel: HTMLCanvasElement, cellSize: number, snake: Snake) {
+    // We only need to paint the head because the rest will have been already painted.
+    paintCell(panel, "green", cellSize, snake.head);
+}
+
+function paintGame(game: Game) {
+    paintApple(game.panel, game.cellSize, game.apple);
+    paintSnake(game.panel, game.cellSize, game.snake);
+}
+
+function newApple(game: Game) {
+    eraseApple(game);
+    return createRandomCell(boardDimensions(game.panel, game.cellSize));
+}
+
+var deltaVector = new Array();
+deltaVector[Direction.LEFT] = { dx: -1, dy: 0 };
+deltaVector[Direction.RIGHT] = { dx: 1, dy: 0 };
+deltaVector[Direction.UP] = { dx: 0, dy: -1 };
+deltaVector[Direction.DOWN] = { dx: 0, dy: 1 };
+
+// Gets a vector containing dx and dy values for a given direction
+function delta(direction: Direction): { dx: number; dy: number } {
+    return deltaVector[direction];
+}
+
+// Returns the snake's direction, which is either the current direction or a new one if a board edge was reached.
+// NOTE: or this may indicate the end of the game. No?
+function newDirection(game: Game) {
+    var snake = game.snake;
+    var direction = snake.direction;
+    var head = snake.head;
+    var x = head.x;
+    var y = head.y;
+    // TODO: this is annoying. This should be cached in a game's property.
+    var dimensions = boardDimensions(game.panel, game.cellSize);
+    var atLeft = (x === 0);
+    var atRight = (x === (dimensions.width - 1));
+    var atTop = (y === 0);
+    var atBottom = (y === (dimensions.height - 1));
+
+    // Turn clockwise when a board edge is reached unless that would result in going off the board
+    if (direction === Direction.UP && atTop) {
+        return atRight ? Direction.LEFT : Direction.RIGHT;
     }
 
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = color;
-    ctx.fillRect(cellSize * cell.x, cellSize * cell.y, cellSize, cellSize);
+    if (direction === Direction.RIGHT && atRight) {
+        return atBottom ? Direction.UP : Direction.DOWN;
+    }
+
+    if (direction === Direction.DOWN && atBottom) {
+        return atLeft ? Direction.RIGHT : Direction.LEFT;
+    }
+
+    if (direction === Direction.LEFT && atLeft) {
+        return atTop ? Direction.DOWN : Direction.UP;
+    }
+
+    return direction;
 }
+
+function isSameOrAdjacentCell(cell1: Cell, cell2: Cell) {
+    var dx = Math.abs(cell1.x - cell2.x);
+    var dy = Math.abs(cell2.y - cell2.y);
+
+    return dx <= 1 && dy <= 1;
+}
+
+function shouldEatApple(game: Game) {
+    return isSameOrAdjacentCell(game.snake.head, game.apple);
+}
+
+function removeTail(game: Game, body: Cell[]) {
+    eraseCell(game, game.snake.tail);
+    return body.slice(0, body.length - 1);
+}
+
+// Moves the snake and returns a new snake class. The snake grows by one cell if grow is true.
+function moveSnake(game: Game, grow: boolean) {
+    var direction = newDirection(game);
+    var d = delta(direction);
+    var head = game.snake.head;
+    var newHead = new Cell(head.x + d.dx, head.y + d.dy);
+    var body = game.snake.body.concat([newHead], game.snake.body);
+    if (!grow) {
+        body = removeTail(game, body);
+    }
+
+    return new Snake(body, direction);
+}
+
+// Gets a direction that describes the direction associated with a given key code.
+function getKeyDirection(keyCode: number): Direction {
+    switch (keyCode) {
+        case 37: return Direction.LEFT;
+        case 38: return Direction.UP;
+        case 39: return Direction.RIGHT;
+        case 40: return Direction.DOWN;
+    }
+    return Direction.NONE;
+}
+
+function isValidChange(keyDirection: Direction, currentDirection: Direction): boolean {
+    switch (keyDirection) {
+        case Direction.NONE: return false;
+        case Direction.LEFT: return currentDirection !== Direction.RIGHT;
+        case Direction.RIGHT: return currentDirection !== Direction.LEFT;
+        case Direction.UP: return currentDirection !== Direction.DOWN;
+        case Direction.DOWN: return currentDirection !== Direction.UP;
+    }
+
+    return true;
+}
+
+var globalKeyCode: number;
+
+function snakeWithKeyDirection(snake: Snake) : Snake {
+    var keyDirection = getKeyDirection(globalKeyCode);
+    var current = snake.direction;
+
+    // Don't let the snake double back on itself.
+    if (isValidChange(keyDirection, current)) {
+        globalKeyCode = null;
+        snake.direction = keyDirection;
+    }
+
+    return snake;
+}
+
+function doesHeadOverlapBody(body: Cell[]) : boolean {
+    var head = body[0];
+    for (var i = 1; i < body.length; i++) {
+        if (head === body[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function restartGame(game: Game) : Game {
+    eraseApple(game);
+    eraseSnake(game);
+    return createGame(game.panel, game.cellSize);
+}
+
+function newGame(game: Game, message: string): Game {
+    var panel = game.panel;
+    //alert(message);
+    //return restartGame(game);
+}
+
+function didWin(game: Game): boolean {
+    return (game.snake.body.length === game.lengthToWin);
+}
+
+function didLose(game: Game): boolean {
+    return doesHeadOverlapBody(game.snake.body);
+}
+
+function step(game: Game) : Game {
+    var eat = shouldEatApple(game);
+    var snake = snakeWithKeyDirection(game.snake);
+    game.snake = snake;
+    if (eat) {
+        game.apple = newApple(game);
+    }
+    snake = moveSnake(game, eat);
+
+    if (didLose(game)) {
+        return newGame(game, "You killed the snake!");
+    } else if (didWin) {
+        return newGame(game, "You win!");
+    } else {
+        game.snake = snake;
+    }
+
+    return game;
+}
+
+function configurePanel(panel: HTMLCanvasElement) {
+    $(panel)
+        .attr("tabindex", "0")
+        .on("keydown", function (ev: KeyboardEvent) {
+            globalKeyCode = ev.keyCode;
+            return false;
+        });
+}
+
+class Greeter {
+    span: HTMLElement;
+    timerToken: number;
+    game: Game;
+
+    constructor(public element: HTMLElement) {
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+
+        var gameBoard = document.getElementById(GameBoardId);
+
+        var cellSize = 20;
+        this.game = createGame(<HTMLCanvasElement>document.getElementById(GameBoardId), cellSize);
+        paintGame(this.game);
+    }
+
+    start() {
+        this.timerToken = setInterval(() => {
+            step(this.game);
+        }, 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+}
+
+var greeter;
 
 window.onload = () => {
     var el = document.getElementById("content");
-    var greeter = new Greeter(el);
+    greeter = new Greeter(el);
     greeter.start();
 };
